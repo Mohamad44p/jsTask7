2e8cbbaa29735c6ad84d36ad2202e30b7e7cf4fb
Mohamad44p <mohamad44p@yahoo.com>
2024-02-03 19:28:50 +0200

    Done

--- script.js
+++ script.js
@@ -1,62 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
-  loadTasks();
-});
-
-function addTask() {
-  var taskInput = document.getElementById("taskInput");
-  var taskText = taskInput.value.trim();
-
-  if (taskText !== "") {
-      var listItem = document.createElement("li");
-      listItem.innerText = taskText;
-
-      var removeButton = document.createElement("button");
-      removeButton.innerHTML = '<span class="box">delete!</span>';
-      removeButton.onclick = function() {
-          listItem.remove();
-          saveTasks();
-      };
-      listItem.appendChild(removeButton);
-
-      var tasksList = document.getElementById("tasks");
-      tasksList.appendChild(listItem);
-
-      taskInput.value = "";
-
-      saveTasks();
-  }
+function openNav() {
+  document.getElementById("sidebar").style.width = "250px";
+  document.getElementById("main-content").style.marginLeft = "250px";
 }
 
-function saveTasks() {
-  var tasks = Array.from(document.getElementById("tasks").children);
-
-  var tasksText = tasks.map(function(task) {
-      return task.innerText.replace("delete!", "").trim();
-  });
-
-  localStorage.setItem("tasks", JSON.stringify(tasksText));
-}
-
-function loadTasks() {
-  var tasksText = localStorage.getItem("tasks");
-
-  if (tasksText) {
-      var tasksArray = JSON.parse(tasksText);
-
-      tasksArray.forEach(function(taskText) {
-          var listItem = document.createElement("li");
-          listItem.innerText = taskText;
-
-          var removeButton = document.createElement("button");
-          removeButton.innerHTML = '<span class="box">delete!</span>';
-          removeButton.onclick = function() {
-              listItem.remove();
-              saveTasks();
-          };
-          listItem.appendChild(removeButton);
-
-          var tasksList = document.getElementById("tasks");
-          tasksList.appendChild(listItem);
-      });
-  }
+function closeNav() {
+  document.getElementById("sidebar").style.width = "0";
+  document.getElementById("main-content").style.marginLeft = "0";
 }
